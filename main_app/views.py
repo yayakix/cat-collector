a74422c362c2c0f6c6c54fc981dd620d338683c2
Iyana Marquez <yayakix@gmail.com>
2022-07-05 12:24:02 -0400

    first commit home and about pages

---
+++ main_app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+# Add new view
+
+# Add the Cat class & list and view function below the imports
+class Cat:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'foul little demon', 3),
+  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
+def cats_index(request):
+  return render(request, 'cats/index.html', { 'cats': cats })
